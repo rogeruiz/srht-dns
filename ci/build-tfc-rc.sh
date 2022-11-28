276dc715d2976ead6eb4db1d6cbc225066ab64c5
Roger Steve Ruiz <hi@rog.gr>
2022-11-28 13:55:51 -0500

    Adding some needed documentation

--- ci/build-tfc-rc.sh
+++ ci/build-tfc-rc.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 set +x
+
+# README: I really don't want to do this but
+# I don't seem to have a choice here. The TF
+# CLI doesn't work with the
+# `TF_TOKEN_app_terraform_io` variable.
+
 cat << TFCRC > ~/.terraformrc
 credentials "app.terraform.io" {
   token = "$(cat ~/.user-token-tf-gandi-dns)"
