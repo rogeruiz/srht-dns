6afe965def7ea68e92163975eb5538653ce19b15
Roger Steve Ruiz <hi@rog.gr>
2023-10-03 14:18:42 -0400

    Actualizando los SRV records de DNS para rog.g

--- rog_gr.tf
+++ rog_gr.tf
@@ -2,6 +2,56 @@ resource "gandi_livedns_domain" "rog_gr" {
   name = "rog.gr"
 }
 
+resource "gandi_livedns_record" "rog_gr_imaptcp_srv" {
+  zone = resource.gandi_livedns_domain.rog_gr.id
+  name = "_imap._tcp"
+  type = "SRV"
+  ttl  = 10800
+  values = [
+    "0 0 0   .",
+  ]
+}
+
+resource "gandi_livedns_record" "rog_gr_imapstcp_srv" {
+  zone = resource.gandi_livedns_domain.rog_gr.id
+  name = "_imaps._tcp"
+  type = "SRV"
+  ttl  = 10800
+  values = [
+    "0 1 993 mail.gandi.net.",
+  ]
+}
+
+resource "gandi_livedns_record" "rog_gr_pop3_srv" {
+  zone = resource.gandi_livedns_domain.rog_gr.id
+  name = "_pop3._tcp"
+  type = "SRV"
+  ttl  = 10800
+  values = [
+    "0 0 0   .",
+  ]
+}
+
+resource "gandi_livedns_record" "rog_gr_pop3s_srv" {
+  zone = resource.gandi_livedns_domain.rog_gr.id
+  name = "_pop3s.tcp"
+  type = "SRV"
+  ttl  = 10800
+  values = [
+    "10 1 995 mail.gandi.net.",
+  ]
+}
+
+resource "gandi_livedns_record" "rog_gr_submission_srv" {
+  zone = resource.gandi_livedns_domain.rog_gr.id
+  name = "_submission._tcp"
+  type = "SRV"
+  ttl  = 10800
+  values = [
+    "0 1 465 mail.gandi.net.",
+  ]
+}
+
 resource "gandi_livedns_record" "rog_gr_gmi_cname" {
   zone = resource.gandi_livedns_domain.rog_gr.id
   name = "gmi"
