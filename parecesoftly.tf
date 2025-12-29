275cdab80d0ea3ec9aa6ac44e6ff45bd43134aab
Roger Steve Ruiz <hi@rog.gr>
2025-12-29 09:57:31 -0600

    Actualizando parecesoftly pa que use Forward Email

--- parecesoftly.tf
+++ parecesoftly.tf
@@ -11,3 +11,24 @@ resource "gandi_livedns_record" "parecesoftly_root_a" {
     "46.23.81.157",
   ]
 }
+
+resource "gandi_livedns_record" "parecesoftly_root_mx" {
+  zone = resource.gandi_livedns_domain.parecesoftly.id
+  name = "@"
+  type = "MX"
+  ttl = 3600
+  values = [
+    "10 mx1.forwardemail.net.",
+    "10 mx2.forwardemail.net."
+  ]
+}
+
+resource "gandi_livedns_record" "parecesoftly_root_txt" {
+  zone = resource.gandi_livedns_domain.parecesoftly.id
+  name = "@"
+  type = "TXT"
+  ttl  = 3600
+  values = [
+    "forward-email-site-verification=rKjvhnw7RG",
+  ]
+}
